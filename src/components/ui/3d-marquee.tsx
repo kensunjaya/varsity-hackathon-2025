4ca42da54c73a1f51e442540c6b4fe03e5c03b8b
Joshua Wenata <100125029+joshuawenata@users.noreply.github.com>
2025-03-23 23:14:53 +0700

    feat: hero page

---
+++ src/components/ui/3d-marquee.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { motion } from "motion/react";
+import { cn } from "@/lib/utils";
+
+export const ThreeDMarquee = ({
+  images,
+  className,
+}: {
+  images: string[];
+  className?: string;
+}) => {
+  const chunkSize = Math.ceil(images.length / 8);
+  const chunks = Array.from({ length: 8 }, (_, colIndex) => {
+    const start = colIndex * chunkSize;
+    return images.slice(start, start + chunkSize);
+  });
+  return (
+    <div
+      className={cn(
+        "mx-auto block h-[1000px] overflow-hidden rounded-2xl max-sm:h-200",
+        className
+      )}
+    >
+      <div className="flex size-full items-center justify-center">
+        <div className="size-[1900px] ml-[135rem] mb-[40rem] shrink-0 scale-50 sm:scale-75 lg:scale-100">
+          <div className="relative top-96 right-[50%] grid size-full origin-top-left grid-cols-8 gap-8 transform-3d">
+            {chunks.map((subarray, colIndex) => (
+              <motion.div
+                animate={{ y: colIndex % 2 === 0 ? 100 : -100 }}
+                transition={{
+                  duration: colIndex % 2 === 0 ? 10 : 15,
+                  repeat: Infinity,
+                  repeatType: "reverse",
+                }}
+                key={colIndex + "marquee"}
+                className="flex flex-col items-start gap-8"
+              >
+                <div />
+                {subarray.map((image, imageIndex) => (
+                  <div className="relative" key={imageIndex + image}>
+                    <div />
+                    <motion.img
+                      whileHover={{
+                        y: -10,
+                      }}
+                      transition={{
+                        duration: 0.3,
+                        ease: "easeInOut",
+                      }}
+                      key={imageIndex + image}
+                      src={image}
+                      alt={`Image ${imageIndex + 1}`}
+                      className="aspect-[490/620] rounded-lg object-cover hover:shadow-2xl"
+                      width={490}
+                      height={620}
+                    />
+                  </div>
+                ))}
+              </motion.div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
