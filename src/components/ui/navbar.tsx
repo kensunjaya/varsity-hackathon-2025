9937d379709ee3aaebe9de5f1215c36fe5a8f4a1
Heryan Djaruma <heryandjaruma@gmail.com>
2025-03-27 15:27:27 +0700

    improve campaign detail

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 import React from "react";
+import Link from "next/link";
 
 const Navbar = ({ pageName }: { pageName: string }) => {
   const router = useRouter();
+
+  const pathname = usePathname()
+  const lastPath = pathname.split("/").filter(Boolean).pop()
 
   const handleNavigation = (path: string) => {
     router.push(path);
@@ -14,83 +18,87 @@ const Navbar = ({ pageName }: { pageName: string }) => {
   return (
     <nav className="sticky z-50 top-10">
       <div className="flex items-center">
-        <button onClick={() => handleNavigation("/")}>
+          <Link href="/">
+            <Image
+              src="/LOGO.svg"
+              alt=""
+              className="absolute ml-20 flex justify-center items-start h-16 w-16"
+              width={100}
+              height={100}
+            />
+          </Link>
+      </div>
+
+      {lastPath !== 'login' && (
+        <div className="flex justify-center items-center translate-y-8 mr-4">
           <Image
-            src="/LOGO.svg"
-            alt=""
-            className="absolute ml-20 flex justify-center items-start h-16 w-16"
+            src="/navbar-back.svg"
+            alt="navbar-bg"
+            className="absolute flex justify-center w-xl"
             width={100}
             height={100}
           />
-        </button>
-      </div>
-      <div className="flex justify-center items-center translate-y-8 mr-4">
-        <Image
-          src="/navbar-back.svg"
-          alt="navbar-bg"
-          className="absolute flex justify-center w-xl"
-          width={100}
-          height={100}
-        />
-        <ul className="flex justify-evenly items-center absolute w-lg">
-          <li className="flex">
-            <button onClick={() => handleNavigation("/campaigns")}>
+          <ul className="flex justify-evenly items-center absolute w-lg">
+            <li className="flex">
+              <button onClick={() => handleNavigation("/campaigns")}>
+                <Image
+                  src="/campaign-list-logo.svg"
+                  alt="leaderboard"
+                  className="w-6"
+                  width={120}
+                  height={120}
+                />
+              </button>
+            </li>
+            <li className="flex">
+              <button onClick={() => handleNavigation("/#impacts")}>
+                <Image
+                  src="/globe-logo.svg"
+                  alt="globe"
+                  className="w-6"
+                  width={120}
+                  height={120}
+                />
+              </button>
+            </li>
+            <li className="flex">
+              <button className="flex items-center justify-center">
+                <Image
+                  src="/home-button.svg"
+                  alt="home-btn"
+                  className="w-2xs"
+                  width={120}
+                  height={120}
+                />
+                <h1 className="absolute text-3xl text-black red font-primary">
+                  {pageName}
+                </h1>
+              </button>
+            </li>
+            <li className="flex">
+              <button onClick={() => handleNavigation("/login")}>
+                <Image
+                  src="/account-logo.svg"
+                  alt="account"
+                  className="w-5"
+                  width={120}
+                  height={120}
+                />
+              </button>
+            </li>
+            <li className="flex">
               <Image
-                src="/campaign-list-logo.svg"
-                alt="leaderboard"
+                src="/login-logo.svg"
+                alt="login"
                 className="w-6"
                 width={120}
                 height={120}
               />
-            </button>
-          </li>
-          <li className="flex">
-            <button onClick={() => handleNavigation("/#impacts")}>
-              <Image
-                src="/globe-logo.svg"
-                alt="globe"
-                className="w-6"
-                width={120}
-                height={120}
-              />
-            </button>
-          </li>
-          <li className="flex">
-            <button className="flex items-center justify-center">
-              <Image
-                src="/home-button.svg"
-                alt="home-btn"
-                className="w-2xs"
-                width={120}
-                height={120}
-              />
-              <h1 className="absolute text-3xl text-black red font-primary">
-                {pageName}
-              </h1>
-            </button>
-          </li>
-          <li className="flex">
-            <button onClick={() => handleNavigation("/login")}>
-              <Image
-                src="/account-logo.svg"
-                alt="account"
-                className="w-5"
-                width={120}
-                height={120}
-              />
-            </button>
-          </li>
-          <li className="flex">
-            <Image
-              src="/login-logo.svg"
-              alt="login"
-              className="w-6"
-              width={120}
-              height={120}
-            />
-          </li>
-        </ul>
-      </div>
+            </li>
+          </ul>
+        </div>
+      )}
+
     </nav>
   );
 };
--- src/pages/LoginWithMetaMask.tsx
+++ src/pages/LoginWithMetaMask.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { ethers } from "ethers";
 import Image from "next/image";
+import {Separator} from "@radix-ui/react-separator";
 
 declare global {
   interface Window {
@@ -36,11 +37,12 @@ const LoginWithMetaMask = () => {
 
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-sixth text-white">
-      <div className="flex flex-col items-center space-y-6">
+      <div className="flex flex-col items-center space-y-6 border-2 border-primary p-5 bg-white rounded-xl">
+        <h1 className={`text-primary text-2xl font-bold`}>Login to DecentraFund</h1>
         <button
           onClick={connectWallet}
-          className="flex items-center gap-3 px-6 py-3 rounded-lg border border-blue-500 text-blue-400 font-semibold transition-all 
-                    bg-white hover:bg-blue-500 hover:text-white active:scale-95"
+          className="flex items-center gap-3 px-6 py-3 rounded-lg border border-primary text-yellow-600 font-semibold transition-all
+                    bg-white hover:bg-yellow-500 hover:text-white active:scale-95"
         >
           <Image
             src="/MetaMask.png"
